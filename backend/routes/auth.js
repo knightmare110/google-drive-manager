5bf06eb9bc078675418197ab5ef5aa0402539b99
knightmare110 <knightmare.protagonist@gmail.com>
2024-09-18 15:33:57 -0400

    Implement more secure token managment way by not exposing in the front end

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,12 +15,42 @@ router.get('/google/callback', async (req, res) => {
   
   try {
     const { tokens } = await oAuth2Client.getToken(code);
+
+    res.cookie('authToken', tokens.access_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: tokens.expiry_date - Date.now()
+    });
+
+    res.cookie('authExpiry', tokens.expiry_date, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: tokens.expiry_date - Date.now()
+    });
+
     oAuth2Client.setCredentials(tokens);
 
-    res.redirect(`http://localhost:3000/?token=${tokens.access_token}`);
+    res.redirect(`http://localhost:3000/`);
   } catch (err) {
     res.status(400).send('Error retrieving tokens');
   }
 });
+
+router.get('/status', async(req, res) => {
+  const token = req.cookies?.authToken || null;
+  const expiry = req.cookies?.authExpiry || null;
+
+  if (!token || !expiry) {
+    return res.json({ loggedIn: false });
+  }
+
+  // Check if the token is expired
+  const currentTime = Date.now();
+  if (currentTime >= parseInt(expiry)) {
+    return res.json({ loggedIn: false });
+  }
+
+  return res.json({ loggedIn: true });
+})
 
 module.exports = router;
